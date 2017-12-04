ca2fa6d9a18d412c8ab28f7804af4940969d2400
shun95ngeggpg <shun.n.95@gmail.com>
2017-12-04 16:26:32 +0900

    precommit edit

--- src/com/internousdev/sukesyunshop/action/GoHomeAction.java
+++ src/com/internousdev/sukesyunshop/action/GoHomeAction.java
@@ -6,7 +6,7 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HomeAction extends ActionSupport implements SessionAware {
+public class GoHomeAction extends ActionSupport implements SessionAware {
 	public Map<String, Object> session;
 
 	public String execute() {
