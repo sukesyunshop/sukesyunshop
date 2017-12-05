2f9c527d05a4fe148d46dfc513faffbd7290eaec
test <test@gmail.com>
2017-12-05 18:23:28 +0900

    a

---
+++ src/com/internousdev/sukesyunshop/action/ResetPasswordAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.sukesyunshop.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ResetPasswordAction extends ActionSupport  {
+
+	public String execute(){
+	return SUCCESS;
+}
+
+
+}
---
+++ src/com/internousdev/sukesyunshop/dto/CompletePasswordDTO.java
@@ -0,0 +1,5 @@
+package com.internousdev.sukesyunshop.dto;
+
+public class CompletePasswordDTO {
+
+}
