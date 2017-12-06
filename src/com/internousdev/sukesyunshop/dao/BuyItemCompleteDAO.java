061c6f9e9b802ee3963bb440ca2a182b6a5a1317
internousdev <internousdev@00174045111205X>
2017-12-06 11:49:54 +0900

    UserInfoDTO

---
+++ src/com/internousdev/sukesyunshop/dao/BuyItemCompleteDAO.java
@@ -0,0 +1,13 @@
+package com.internousdev.sukesyunshop.dao;
+
+import java.sql.Connection;
+
+import com.internousdev.sukesyunshop.util.DBConnector;
+
+public class BuyItemCompleteDAO {
+
+	private DBConnector db = new DBConnector();
+	private Connection con = db.getConnection();
+
+
+}
