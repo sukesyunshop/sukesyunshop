4149991f322d1a3456478c6c5cee1ef8d5759123
shun95ngeggpg <shun.n.95@gmail.com>
2017-12-08 12:42:12 +0900

    edit sql file

--- sql/sukesyunshop.sql
+++ sql/sukesyunshop.sql
@@ -6,7 +6,7 @@ use sukesyunshopdb;
 
 CREATE TABLE user_info(
 id int not null primary key auto_increment,
-user_id varchar(16) unique,
+user_id varchar(128) unique,
 password varchar(16),
 family_name varchar(16),
 first_name varchar(16),
@@ -18,7 +18,8 @@ status tinyint(1) default 1,
 logined tinyint(1) default 0,
 insert_date datetime default NOW(),
 update_date datetime
-);
+)
+comment='会員情報テーブル';
 
 CREATE TABLE m_category(
 id int not null primary key auto_increment,
@@ -27,7 +28,8 @@ category_name varchar(20)unique,
 category_description varchar(100),
 insert_date datetime default NOW(),
 update_date datetime
-);
+)
+comment='カテゴリマスタテーブル';
 
 CREATE TABLE product_info(
 id int primary key auto_increment,
@@ -46,7 +48,8 @@ insert_date datetime default NOW(),
 update_date datetime,
 foreign key(category_id) references m_category(category_id)
 ON UPDATE CASCADE ON DELETE CASCADE
-);
+)
+comment='商品情報テーブル';
 
 CREATE TABLE cart_info(
 id int primary key auto_increment,
@@ -58,11 +61,12 @@ foreign key(user_id) references user_info(user_id)
 ON UPDATE CASCADE ON DELETE CASCADE,
 foreign key(product_id) references product_info(product_id)
 ON UPDATE CASCADE ON DELETE CASCADE
-);
+)
+comment='カート情報テーブル';
 
 CREATE TABLE purchase_history_info(
 id int primary key auto_increment,
-user_id varchar(16),
+user_id varchar(128),
 product_id int,
 insert_date datetime default NOW(),
 update_date datetime,
@@ -70,11 +74,12 @@ foreign key(user_id) references user_info(user_id)
 ON UPDATE CASCADE ON DELETE CASCADE,
 foreign key(product_id) references product_info(product_id)
 ON UPDATE CASCADE ON DELETE CASCADE
-);
+)
+comment='購入履歴情報テーブル';
 
 CREATE TABLE destination_info(
 id int primary key auto_increment,
-user_id varchar(16),
+user_id varchar(128),
 family_name varchar(16),
 first_name varchar(16),
 family_name_kana varchar(16),
@@ -87,4 +92,5 @@ insert_date datetime default NOW(),
 update_date datetime,
 foreign key(user_id) references user_info(user_id)
 ON UPDATE CASCADE ON DELETE CASCADE
-);
+)
+comment='宛先情報テーブル';
